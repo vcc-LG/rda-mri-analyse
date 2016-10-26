c19aa932d17ae70ab5669b52226c64930f934ab4
vcc-LG <leo.garcia2@wales.nhs.uk>
2016-10-26 14:39:21 +0100

    database probably working

--- main.py
+++ main.py
@@ -2,25 +2,93 @@
 
 import os, datetime
 import subprocess
+import sqlite3
 
-def call_tarquin(input_path,my_file_name,my_output_path):
+
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+    return None
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except sqlite3.Error as e:
+        print(e)
+
+def create_database(database_path):
+    conn = create_connection(database_path)
+    f = open('sql\\sql_create_table.sql', 'r')
+    sql = f.read()
+    if conn is not None:
+        create_table(conn, sql)
+    else:
+        print("Error! cannot create the database connection.")
+
+def insert_into_database(database_path,my_file_name_strip,raw_data,time_stamp):
+    conn = create_connection(database_path)
+    f = open('sql\\sql_insert_data.sql', 'r')
+    sql = f.read()
+    place_holders = '?,'*(len(raw_data)+2)
+    place_holders = place_holders[:-1]
+    conc_data = []
+
+    for row in raw_data:
+        try:
+            conc_data.append(float(row.split()[1]))
+        except:
+            conc_data.append('NULL')
+    insert_data = [my_file_name_strip] + [time_stamp] + conc_data
+    insert_sql = sql+place_holders+")"
+    try:
+        c = conn.cursor()
+        c.execute(insert_sql,insert_data)
+    except sqlite3.Error as e:
+        print(e)
+
+def get_tarquin_data(output_txt_path):
+    with open(output_txt_path, "r") as ins:
+        all_data = []
+        for line in ins:
+            if '--------' in line:
+                break
+        for line in ins:
+            if not line.strip():
+                break
+            all_data.append(line)
+    return all_data
+
+def call_tarquin(input_path,my_file_name,my_output_path,database_path):
     my_file_name_strip = my_file_name.strip('.rda')
-    output_dir = os.path.join('output\\',my_file_name_strip+'_'+datetime.datetime.now().strftime('%Y%m%d_%H%M'))
+    time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+    time_stamp_dir = time_stamp
+    time_stamp_dir = time_stamp_dir.replace(' ','_')
+    time_stamp_dir = time_stamp_dir.replace(':','')
+
+    output_dir = os.path.join('output\\',my_file_name_strip+'_'+time_stamp_dir)
     os.makedirs(output_dir,
                 exist_ok=True)
+    output_txt_path = os.path.join(output_dir,my_file_name_strip+'.txt')
     tarquin_cmd = [r'tarquin\\tarquin\\tarquin.exe',
                    '--input',
                    input_file_path + file_name,
                    '--format','rda',
                    '--output_txt',
-                   my_output_path+my_file_name.strip('.rda')+'.txt']
+                   output_txt_path]
     subprocess.call(tarquin_cmd)
-    os.rename("plot.txt",os.join(output_dir,"plot.txt"))
-    os.rename("results.txt", os.join(output_dir, "results.txt"))
-    os.rename("gnuplot.txt", os.join(output_dir, "gnuplot.txt"))
+    raw_data = get_tarquin_data(output_txt_path)
+    insert_into_database(database_path,my_file_name_strip,raw_data,time_stamp)
 
 input_file_path = r'data\\'
 output_file_path = r'output\\'
+database_file_path = r'database\\'
+database_file_name = 'rda_database2.db'
+database_full_path = os.path.join(database_file_path,database_file_name)
+create_database(database_full_path)
 
 for file_name in os.listdir(input_file_path):
-    call_tarquin(input_file_path,file_name,output_file_path)
+    call_tarquin(input_file_path,file_name,output_file_path,database_full_path)
